1dd9f4106b0a48303e75f499cc46f7ac6fb830ec
Liang LIu <lliu@uga.edu>
2021-02-12 07:26:14 -0500

    Update tree.consensus.R

--- R/tree.consensus.R
+++ R/tree.consensus.R
@@ -1,8 +1,8 @@
 'tree.consensus'<-
 function(treefile, outfile, rooted=FALSE, sumtreepath="sumtrees.py")
 {
-    command <- paste("sumtrees.py -f0 -p ", treefile, " -d0 --replace -F newick -o", outfile, " --no-annotations")
-    if(rooted) paste("sumtrees.py -f0 -p ", treefile, " -d0 --replace -F newick -o", outfile, " --no-annotations --rooted")
+    command <- paste(sumtreepath," -f0 -p ", treefile, " -d0 --replace -F newick -o", outfile, " --no-annotations")
+    if(rooted) paste(sumtreepath," -f0 -p ", treefile, " -d0 --replace -F newick -o", outfile, " --no-annotations --rooted")
     try(system(command))
     contree <- read.tree(outfile)
     con = write.tree(contree)
