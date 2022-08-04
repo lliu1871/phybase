6f7961a40c3b3d9a369e5bc9c6274fb39aa73e07
Liang Liu <lliu@uga.edu>
2022-08-04 13:57:28 -0400

    Update tree.probdist.R

--- R/tree.probdist.R
+++ R/tree.probdist.R
@@ -1,20 +1,17 @@
 'tree.probdist'<-
 function(treefile, sumtreepath="sumtrees.py")
 {
-    command<-paste(sumtreepath, treefile, "--trprobs=post4342p")
+    command<-paste(sumtreepath, treefile, "-x=post4342p")
     try(system(command))
-    x<-scan("post4342p",what="character",sep="\n")
-    trees<-x[grep("TREE Tree",x)]
+    x<-scan("post4342p.topologies.trees",what="character",sep="\n")
+    trees<-x[grep("TREE ",x)]
     t<-matrix(unlist(strsplit(trees,split="]")),nrow=2)[2,]
-    prob<-matrix(unlist(strsplit(trees,split="]")),nrow=2)[1,]
-    y<-unlist(strsplit(prob,split=",probability="))
-    y<-y[grep("cumulative",y)]
-    y<-unlist(strsplit(y,split=",cumulative"))
-    prob<-as.numeric(y[-grep("probability",y)])
-    try(system("rm -f post4342p"))
+    prob<-as.numeric(gsub("\\[&W","",matrix(t,2)[1,]))
+    try(system("rm -f post4342p*"))
     result<-c(tree=as.character, prob=as.numeric)
-    result$tree<-t
+    result$tree<-matrix(t,2)[2,]
     result$prob<-prob
     result
 }
+
 
